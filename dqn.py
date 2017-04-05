removed autodiff, added hard-coded derivative, fixed speed

dqn.py
@@ -2,12 +2,12 @@ import itertools
 import sys
 import time
 
-import numpy as np
-import gym.spaces
-
 from typing import Callable
 from typing import Dict
 from typing import Tuple
+
+import numpy as np
+import gym.spaces
 
 from gym import wrappers
 
@@ -109,19 +109,29 @@ def learn(env,
             learning_rate: float,
             model_curr: Dict,
             model_target: Dict) -> Dict:
-        """Train function, minimizing loss per q-learning objective."""
+        """Train function, minimizing loss per q-learning objective.
+
+        This assumes the q_function is a one-layer fc neural network, where the
+        loss function is squared error.
+        """
+
         curr_q = q_func(obs_t, model_curr)
         target_q = q_func(obs_tp1, model_target)
         actions = one_hot(act_t, num_actions)
         q_target_max = np.max(target_q, axis=1)
         q_target_val = rew_t + gamma * (1. - done_mask) * q_target_max
         q_candidate_val = np.sum(curr_q * actions, axis=1)
-        total_error = sum((q_target_val - q_candidate_val)**2)
+        _ = sum((q_target_val - q_candidate_val) ** 2)
 
-        for k, W in model_curr.items():
-            for i, row in enumerate(W):
-                for j, entry in enumerate(row):
-                    W[i][j] += learning_rate * total_error.d(entry)
+        d = obs_t.shape[1] * obs_t.shape[2] * obs_t.shape[3]
+        grad_loss = 2*(q_target_val - q_candidate_val).reshape((-1, 1))
+        obs_t = obs_t.reshape([-1, d])
+        gradient = np.zeros(model_curr['W0'].shape)
+        for grad_loss, x, action in zip(grad_loss, obs_t, actions):
+            x = x.reshape((x.shape[0], 1))
+            action = action.reshape((1, action.shape[0]))
+            gradient += np.asscalar(grad_loss) * x.dot(action)
+        model_curr['W0'] += learning_rate * gradient
         return model_curr
 
 
@@ -138,6 +148,7 @@ def learn(env,
     learning_rate = exploration.value(0)
     model_curr = {}
     model_target = {}
+    start_time = None
 
     for t in itertools.count():
 
@@ -185,7 +196,8 @@ def learn(env,
                 done_mask=done_mask,
                 learning_rate=learning_rate,
                 model_curr=model_curr,
-                model_target=model_target)
+                model_target=model_target
+            )
 
             if t % target_update_freq == 0:
                 update_target_func(model_curr, model_target)
@@ -200,7 +212,9 @@ def learn(env,
             best_mean_episode_reward = max(best_mean_episode_reward,
                                            mean_episode_reward)
         if t % LOG_EVERY_N_STEPS == 0 and model_initialized:
-            print("Time %s s" % int(time.time() - start_time))
+            if start_time is not None:
+                print("Time %s s" % int(time.time() - start_time))
+            start_time = time.time()
             print("Timestep %d" % t)
             print("mean reward (100 episodes) %f" % mean_episode_reward)
             print("best mean reward %f" % best_mean_episode_reward)

run_dqn.py
@@ -19,8 +19,8 @@ import docopt
 import gym
 import numpy as np
 
-from ad import adnumber
 from gym import wrappers
+from numba import jit
 from skimage.transform import pyramid_reduce
 
 import os.path
@@ -33,14 +33,15 @@ from typing import Tuple
 def initialize_model(input_shape: Tuple, num_actions: int) -> Dict:
     """Initialize the model"""
     h, w, d = input_shape
-    return {'W0': adnumber(np.random.random((h * w * d, num_actions)))}
+    return {'W0': np.random.random((h * w * d, num_actions))}
 
 
-def evaluate(X: np.ndarray, model) -> np.ndarray:
+@jit
+def evaluate(X: np.ndarray, model: Dict) -> np.ndarray:
     """Evaluate the neural network."""
     W0 = model['W0']
     l0 = X.reshape((X.shape[0], W0.shape[0]))
-    l1 = np.array([sigmoid(l0.dot(w)) for w in W0.T])
+    l1 = np.maximum(0, l0.dot(W0))
     return l1
 
 
@@ -50,11 +51,6 @@ def featurize(X: np.ndarray, target=(84, 84, 4)) -> np.ndarray:
     Simple image compression, for now.
     """
     return pyramid_reduce(X, scale=X.shape[0]/target[0])
-
-
-def sigmoid(x):
-    """Sigmoid activation function."""
-    return 1/(1+np.exp(-x))
 
 
 def set_global_seeds(i):